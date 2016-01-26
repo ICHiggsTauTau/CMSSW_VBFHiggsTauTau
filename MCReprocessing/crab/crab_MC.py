f5fc2dbe6ef42d157cfd38bf55d488856f5bf0e5
Joao Pela <pela@cern.ch>
2016-01-26 11:41:53 +0100

    Adding white list

--- MCReprocessing/crab/crab_MC.py
+++ MCReprocessing/crab/crab_MC.py
@@ -1,20 +1,21 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'VBFHiggsTauTau'
-config.General.workArea = 'Reprocessing_RAW_v1'
+config.General.requestName     = 'VBFHiggsTauTau'
+config.General.workArea        = 'Reprocessing_RAW_v2'
 config.General.transferOutputs = True
-config.General.transferLogs    = False
+config.General.transferLogs    = True
 
 config.JobType.pluginName = 'ANALYSIS'
 config.JobType.psetName   = 'make_GEN-SIM-RAW_likeTSG_cfg.py'
 
-config.Data.inputDataset  = '/VBFHToTauTau_M125_13TeV_powheg_pythia8/RunIISummer15GS-MCRUN2_71_V1-v1/GEN-SIM'
-config.Data.inputDBS      = 'global'
-config.Data.splitting     = 'FileBased'
-config.Data.unitsPerJob   = 1
-config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
-config.Data.publication   = True
-config.Data.outputDatasetTag = 'Reprocessing_VBFHiggsTauTau_RAW_20160122'
+config.Data.inputDataset     = '/VBFHToTauTau_M125_13TeV_powheg_pythia8/RunIISummer15GS-MCRUN2_71_V1-v1/GEN-SIM'
+config.Data.inputDBS         = 'global'
+config.Data.splitting        = 'FileBased'
+config.Data.unitsPerJob      = 1
+config.Data.outLFNDirBase    = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication      = True
+config.Data.outputDatasetTag = 'Reprocessing_VBFHiggsTauTau_RAW_v2'
 
-config.Site.storageSite     = 'T2_UK_London_IC'
+config.Site.whitelist   = ["T2_UK_London_IC"]
+config.Site.storageSite = 'T2_UK_London_IC'
