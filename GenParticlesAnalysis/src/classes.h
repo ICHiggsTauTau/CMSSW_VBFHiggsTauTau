f6d27c151221608056c0f6a8af4d8c2182fb9e42
Joao Pela <pela@cern.ch>
2016-02-12 17:26:37 +0000

    Adding for making dictionaries for new data formats

---
+++ GenParticlesAnalysis/src/classes.h
@@ -0,0 +1,10 @@
+#include "CMSSW_VBFHiggsTauTau/GenParticlesAnalysis/interface/VBFHiggsToTauTauGenAnalysisDataFormat.h"
+#include "CMSSW_VBFHiggsTauTau/GenParticlesAnalysis/interface/MicroGenParticle.h"
+
+namespace{
+  namespace{
+    VBFHiggsToTauTau::GenAnalysisDataFormat pGenAnalysis; 
+    VBFHiggsToTauTau::MicroGenParticle      pMicroGenPar;
+  }
+}
+ 
