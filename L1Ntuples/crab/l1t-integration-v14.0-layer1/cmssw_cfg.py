5c1e964058cf0b79ecd9ba07187b60bdbb54727e
Joao Pela <pela@cern.ch>
2016-03-22 21:44:00 +0000

    Adding crab cfg for l1t-integration-v14.0-layer1

---
+++ L1Ntuples/crab/l1t-integration-v14.0-layer1/cmssw_cfg.py
@@ -0,0 +1,1 @@
+../../test/l1t-integration-v14.0-layer1/l1Ntuple_RAW2DIGI.py
---
+++ L1Ntuples/crab/l1t-integration-v14.0-layer1/crab_mc_VBFHToTauTau_noHF.py
@@ -0,0 +1,22 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName     = 'VBFHToTauTau'
+config.General.workArea        = 'L1NTuplesV2_l1t-integration-v14.0-layer1_METEtaRange3p0_v1'
+config.General.transferOutputs = True
+config.General.transferLogs    = True
+
+config.JobType.pluginName = 'ANALYSIS'
+config.JobType.psetName   = 'cmssw_cfg.py'
+config.JobType.pyCfgParams = ['sample=mc']
+
+config.Data.inputDataset     = '/VBFHToTauTau_M125_13TeV_powheg_pythia8/pela-Reprocessing_VBFHiggsTauTau_RAW_v3-02e742d2ba16f475bed5477b1348f921/USER'
+config.Data.inputDBS         = 'phys03'
+config.Data.splitting        = 'EventAwareLumiBased'
+config.Data.unitsPerJob      = 50000
+config.Data.outLFNDirBase    = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication      = False
+config.Data.outputDatasetTag = 'L1NTuplesV2_l1t-integration-v14.0-layer1_METEtaRange3p0_v1'
+
+#config.Site.whitelist   = ["T2_UK_London_IC"]
+config.Site.storageSite = 'T2_UK_London_IC'
