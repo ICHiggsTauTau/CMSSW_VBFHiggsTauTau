9973cdb2d66e209720cb2b4d0e7b7f0f89d87dd5
Joao Pela <pela@cern.ch>
2016-06-10 14:05:32 +0100

    Script to clean intermediate products

---
+++ L1Analysis/jobs/l1t-tsg-v5/cleanJobProducts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -f task_VBFHToInvisible/output/*.root
+
+rm -f task_VBFHToTauTau_EleHad/output/*.root
+rm -f task_VBFHToTauTau_EleMuo/output/*.root
+rm -f task_VBFHToTauTau_HadHad/output/*.root
+rm -f task_VBFHToTauTau_MuoHad/output/*.root
+
+rm -f task_offline_VBFHToTauTau_EleHad/output/*.root
+rm -f task_offline_VBFHToTauTau_EleMuo/output/*.root
+rm -f task_offline_VBFHToTauTau_HadHad/output/*.root
+rm -f task_offline_VBFHToTauTau_MuoHad/output/*.root
+
+
+rm -f task_ZeroBias/output/*.root
+
