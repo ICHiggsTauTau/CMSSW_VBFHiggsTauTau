2f03a084febbbfb127a124f43a2bbb1b7c55bfd8
Daniel <d.winterbottom15@imperial.ac.uk>
2016-06-15 14:47:24 +0100

    removing task directories and generated job scripts

--- L1Analysis/jobs/l1t-integration-v58.1/task_VBFHToTauTau_MuoHad/jobs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /home/hep/dw515/JoaoOptimisation/CMSSW_8_0_2/src/CMSSW_VBFHiggsTauTau/L1Analysis/jobs/l1t-integration-v58.1/task_VBFHToTauTau_MuoHad/
-source /vols/grid/cms/setup.sh
-eval `scramv1 runtime -sh`
-vbftautau_runAlgoAnalysis --inputType filelist --input /home/hep/dw515/JoaoOptimisation/CMSSW_8_0_2/src/CMSSW_VBFHiggsTauTau/L1Analysis/jobs/l1t-integration-v58.1/task_VBFHToTauTau_MuoHad/jobs/inputFileList_${SGE_TASK_ID}.txt --outputFilename /home/hep/dw515/JoaoOptimisation/CMSSW_8_0_2/src/CMSSW_VBFHiggsTauTau/L1Analysis/jobs/l1t-integration-v58.1/task_VBFHToTauTau_MuoHad/output/results_${SGE_TASK_ID}.root --maxEvents -1 --doAnalysisL1TAlgoScan --jobType mc --decay MuoHad
--- L1Analysis/jobs/l1t-integration-v58.1/task_VBFHToTauTau_MuoHad/submit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-qsub -q hep.q -l h_rt=6:0:0 -t 1-50:1 jobs.sh
